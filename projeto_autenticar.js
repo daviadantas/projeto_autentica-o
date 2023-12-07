3f67614a7d115a8621e71b50d9ac565ee00d1e84
davi3432 <davi.dantas151@gmail.com>
2023-12-06 21:44:05 -0300

    criei a função GetDoc e ReadWorkSheet

--- projeto_autenticar.js
+++ projeto_autenticar.js
@@ -12,3 +12,20 @@ const jwt = new JWT({
     key: credencias.private_key,
     scopes: SCOPES,
 });
+
+async function GetDoc() {
+    const doc = new GoogleSpreasheet(arquivo.id, jwt);
+    await doc.loadInfo();
+    return doc;
+   };
+
+async function ReadWorkSheet(){
+    let sheet = (await GetDoc()).sheetsByIndex[0];
+    let rows = await sheet.getRows()
+    let users = rows.map(row => {
+        return row.toObject()
+    })
+    return users
+}
+
+   
